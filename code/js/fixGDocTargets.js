0fb09919fece67a3a078008083dd701f98fb266a
andruid <andruid@ecologylab.net>
2015-07-20 21:27:32 -0500

    intermediate work on hcc assignments

---
+++ code/js/fixGDocTargets.js
@@ -0,0 +1,27 @@
+function fixGDocTargets(thatFrame)
+{
+    $(function() {
+        $.get(thatFrame.src, function(html) {
+            thatFrame.attr("srcdoc", html);
+            setTimeout(function() {
+               thatFrame.contents().find('a[href^="http"]').attr("target", "_blank");
+            }, 1000);
+        });
+    });
+}
+function fixGDocTargets2(thatFrame)
+{
+	if (!thatFrame.foo)
+	{
+		thatFrame.foo	= true;
+		var jFrame		= $(thatFrame);
+		$(function() {
+		    $.get(thatFrame.src, function(html) {
+		        jFrame.attr("srcdoc", html);
+		        setTimeout(function() {
+		            jFrame.contents().find('a[href^="http"]').attr("target", "_blank");
+		        }, 1000);
+		    });
+		});
+	}
+}
