c59cb8e0c30c13d117df61d34e3b4dd07c6089b5
andruid <andruid@ecologylab.net>
2016-02-17 19:13:37 -0600

    minor courses html examples tweaks

--- courses/html/burnsImage/burnsImage.js
+++ courses/html/burnsImage/burnsImage.js
@@ -14,22 +14,22 @@ function moveToClick(event)
 		x: event.pageX,
 		y: event.pageY
 	};
-	
+
 	var w = document.body.clientWidth;
 	var h = document.documentElement.clientHeight;
-	
+
 	var positionOnImage = {
-		x: mouse.x - image.x,
+		x: image.x - mouse.x,
 		y: image.y - mouse.y
 	};
-	
-	target.x = (w/2) - positionOnImage.x;
-	target.y = positionOnImage.y + (h/2);
-	
+
+	target.x = (w/2) + positionOnImage.x;
+	target.y = (h/2) + positionOnImage.y;
+
 	image.x = target.x;
 	image.y = target.y;
-	
+
 	document.getElementById("mapImage").style.left = image.x + 'px';
 	document.getElementById("mapImage").style.top = image.y + 'px';
-	
+
 }
