73a8d1eac6953178321322e65ca90b43ee346a56
andru1d <andruid@ecologylab.net>
2018-12-03 18:53:21 -0600

    includes iterim

--- includes/ecologylab.js
+++ includes/ecologylab.js
@@ -1,9 +1,34 @@
-    function toggleNav()
-    {
-        var nav     =document.getElementById("navigation_container");
-        var navStyle=nav.style;
-        if (navStyle.visibility == "visible")
-            navStyle.visibility = "hidden";
-        else
-            navStyle.visibility = "visible";
+function toggleNav()
+{
+  var nav     =document.getElementById("navigation_container");
+  var navStyle=nav.style;
+  if (navStyle.visibility == "visible")
+      navStyle.visibility = "hidden";
+  else
+      navStyle.visibility = "visible";
+}
+function handleKey(e)
+{
+  
+}
+function offNav()
+{
+  var thatStyleName = that.style.className;
+  alert(thatStyleName);
+  if (thatStyleName) {
+      var nav     =document.getElementById("navigation_container");
+      var navStyle=nav.style;
+      navStyle.visibility = "hidden";      
     }
+
+}
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  if (keyName == "Escape")
+    {
+      var nav     =document.getElementById("navigation_container");
+      var navStyle=nav.style;
+      navStyle.visibility = "hidden";      
+    }
+ // alert('keydown event\n\n' + 'key: ' + keyName);
+});
